e322a20f9306496a5fd483a9cc3bfb8d2dc8751f
Edd Steel <edward.steel@gmail.com>
2012-05-17 20:36:22 -0700

    fix features script for clients that use them

--- .mutt/features.sh
+++ .mutt/features.sh
@@ -2,11 +2,11 @@
 # config, for if certain features are availble.
 
 if mutt -v | grep -q patched; then
-	echo set sidebar_delim = ' '
-	echo unset sidebar_shortpath
+	echo 'set sidebar_delim = " "'
+	echo 'unset sidebar_shortpath'
 fi
 
-if mutt -v | grep features/xtitles; then
-	set xterm_set_titles
-	set xterm_title="mutt %f (%n/%m)"
+if mutt -v | grep -q 'features/xtitles'; then
+	echo 'set xterm_set_titles'
+	echo 'set xterm_title="mutt %f (%n/%m)"'
 fi
